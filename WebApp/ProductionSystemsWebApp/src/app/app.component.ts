3d66be3419ef8b4885923dcfa18df4539e9f0e8e
macpli <pliszekmaciej0@gmail.com>
2023-11-16 15:24:38 +0100

    add test api call

---
+++ WebApp/ProductionSystemsWebApp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule,RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+}
